0d792852086896d67cc213e50c3b9f0885d74124
Maksymilian Hebda <maksymilian.hebda@gmail.com>
2020-07-31 01:38:51 +0200

    Override i Virtual Nadpisywanie metod

---
+++ nauka C#/05 Virtual i Override/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Punkt a = new Punkt(2, 4);
+        Punkt b = new Punkt3d(1, 2, 3);
+
+        Console.WriteLine(b.wyswietl());
+
+        Console.ReadLine();
+    }
+}
+
