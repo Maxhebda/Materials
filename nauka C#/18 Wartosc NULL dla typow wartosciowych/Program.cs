0a5240f31b238ec74be6f192eee098ee74204e71
Maksymilian Hebda <maksymilian.hebda@gmail.com>
2020-08-01 12:03:17 +0200

    Null dla typow wartosciowych

---
+++ nauka C#/18 Wartosc NULL dla typow wartosciowych/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int a = null;     // nie mozna       
+        int? a = null;      // mozna
+        if (a == null) Console.WriteLine("a jest null");
+        Console.WriteLine(a);   // wyswietla puste
+
+        //int b = (int)a;   // nie mozna Obiekt dopuszczający wartość pustą musi mieć wartość.”
+        int b = a ?? 0;     //mozna  - przypisz a jesli istnieje lub zero jesli nie istnieje
+
+        Console.ReadLine();
+    }
+}
